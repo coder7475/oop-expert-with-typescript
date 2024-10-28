6353c4fbd3ce170bd5396538fe3e5213c504096d
Ahmad Jafari <a99jafari@gmail.com>
2024-10-29 03:15:12 +0330

    Use Bun instead of NodeJS as JavaScript runtime runner

--- script.ts
+++ script.ts
@@ -1,22 +1,6 @@
-import fs from "node:fs";
+import { Glob } from "bun";
+const glob = new Glob("*");
 
-const JOINER = "\n\n[`⬆ BACK TO TOP ⬆`](#table-of-contents)";
-
-const DOCS_DIRECTORY = "./Documentations";
-const subDirectoriesOfDocs = fs.readdirSync(DOCS_DIRECTORY);
-
-const contentOfDocs = subDirectoriesOfDocs.reduce((docs, subDirectory) => {
-  const fileNames = fs.readdirSync(`${DOCS_DIRECTORY}/${subDirectory}`);
-
-  const contents = fileNames.map((fileName) => {
-    const fileContent = fs.readFileSync(`${DOCS_DIRECTORY}/${subDirectory}/${fileName}`, "utf-8");
-
-    return `${fileContent.trim()}${JOINER}`;
-  });
-
-  return [...docs, ...contents];
-}, [] as string[]);
-
-const fullDocumentation = contentOfDocs.join("\n\n");
-
-fs.writeFileSync("./README.md", fullDocumentation);
+for (const file of glob.scanSync("./Documentations")) {
+    console.log(file);
+}
